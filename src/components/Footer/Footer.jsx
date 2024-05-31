a3bac9f6dccce89ee634e56582dac0e4732d9ca2
Seba <seba.penah@gmail.com>
2024-05-31 17:51:46 -0400

    Agregué el Header y Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className='footer-css'>
+      <strong> &copy; 2024 Academia DesafioLatam. Hecho con fines académicos. </strong>
+    </div>
+  )
+}
+
+export default Footer
